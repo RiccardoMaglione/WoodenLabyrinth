146ee72275ecd3ce4a3384e9ce134a51057d3016
Maglione Riccardo <riccardo99maglione@gmail.com>
2020-11-27 18:08:25 +0100

    Add script for movement of ball with accelerometer

---
+++ Assets/Scripts/Accelerometer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerometer : MonoBehaviour
+{
+    public float Speed = 250;
+    public float Sensibility = 5;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer)
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            Vector3 MovementBall = new Vector3(h, 0, v);
+            rb.AddForce(MovementBall * Speed * Time.deltaTime);
+        }
+        if(Application.platform == RuntimePlatform.Android)
+        {
+            Vector3 MovementBall = new Vector3(Input.acceleration.x, 0, Input.acceleration.y);
+            rb.AddForce(MovementBall * Speed * Sensibility * Time.deltaTime);
+        }
+    }
+}
