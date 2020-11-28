e34c4edcd9f997ebc6605a7d212cceeb29daedd7
Maglione Riccardo <riccardo99maglione@gmail.com>
2020-11-28 12:49:12 +0100

    Add script for simulate rotation by accelerometer on pc

---
+++ Assets/Scripts/RotateLabyrinth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLabyrinth : MonoBehaviour
+{
+    public float SpeedRotation = 90;
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        transform.Rotate(Vector3.right * SpeedRotation * Time.deltaTime * v);
+        transform.Rotate(Vector3.back * SpeedRotation * Time.deltaTime * h);
+    }
+}
