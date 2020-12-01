80ced934cff8a4541fa6f917a98fbd23c4a05241
Maglione Riccardo <riccardo99maglione@gmail.com>
2020-12-01 18:48:37 +0100

    Add checkpoint script, versioning text and programmer's name text
    Checkpoint script detect the correct road and add +1 when the player finishes the path

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    #region Variables
+    public int IDCheckpoint;
+    public static int CountCheckpoint;
+    bool isEnter = true;
+    public bool[] InspectorCheckpointArray = new bool[8];
+    public static bool isWin = false;
+    public static bool[] CheckpointArray = new bool[8];
+    #endregion
+
+    private void Start()
+    {
+        for (int i = 0; i < CheckpointArray.Length; i++)
+        {
+            CheckpointArray[i] = false;
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < CheckpointArray.Length; i++)               
+        {
+            if(CheckpointArray[i] == true)                            
+            {
+                if(CheckpointArray[CheckpointArray.Length-1] == true && CountCheckpoint == CheckpointArray.Length)  
+                {
+                    print("Hai vinto");
+                    isWin = true;
+                    for (int k = 0; k < CheckpointArray.Length; k++)    
+                    {
+                        CheckpointArray[IDCheckpoint] = false;       
+                        isEnter = true;
+                        CountCheckpoint = 0;
+                    }
+                }
+            }
+            else
+            {                                                           
+                if(CheckpointArray[CheckpointArray.Length-1] == true)
+                {
+                    for (int j = 0; j < CheckpointArray.Length; j++)
+                    {
+                        CheckpointArray[IDCheckpoint] = false;
+                        isEnter = true;
+                        CountCheckpoint = 0;
+                    }
+                }
+            }
+        }
+        for (int n = 0; n < CheckpointArray.Length; n++)
+        {
+            InspectorCheckpointArray[n] = CheckpointArray[n];
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "BallPlayer" && isEnter == true)
+        {
+            for (int i = 0; i < CheckpointArray.Length; i++)
+            {
+                if(this.IDCheckpoint == i)
+                {
+                    isEnter = false;
+                    CheckpointArray[IDCheckpoint] = true;
+                    CountCheckpoint++;
+                    print(CountCheckpoint);
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class LevelManager : MonoBehaviour
 {
     public GameObject Ball;
     public Material NormalMaterial;
     public Material ShaderXRayMaterial;
     Material[] matArrayStandard;
-
+    public Text CheckpointText;
+    int CountWin;
     private void Update()
     {
         if (PlayerPrefs.GetInt("ActiveShaderXray") == 1)
@@ -25,8 +26,10 @@ public class LevelManager : MonoBehaviour
             matArrayStandard[1] = NormalMaterial;
             Ball.GetComponent<MeshRenderer>().materials = matArrayStandard;
         }
+        DetectWin();
     }
-
+    #region Function and Method
+    #region Scene Management
     public void BackToMenu()
     {
         SceneManager.LoadScene("Menu");
@@ -37,4 +40,17 @@ public class LevelManager : MonoBehaviour
         FallInHole.CountFall = 0;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+    #endregion
+    #region Checkpoint Management
+    public void DetectWin()
+    {
+        if(Checkpoint.isWin == true)
+        {
+            Checkpoint.isWin = false;
+            CountWin++;
+            CheckpointText.text = "Path Finished: " + CountWin.ToString();
+        }
+    }
+    #endregion
+    #endregion
 }
